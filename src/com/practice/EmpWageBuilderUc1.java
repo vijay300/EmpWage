b88e14aa4a52bec69082bf94a0291f6703612642
vijay300 <gnvijayk@gmail.com>
2020-11-14 15:25:30 +0530

    uc4 added as switch case

--- src/com/practice/EmpWageBuilderUc1.java
+++ src/com/practice/EmpWageBuilderUc1.java
@@ -1,28 +1,32 @@
 package com.practice;
 
 public class EmpWageBuilderUc1 {
+	public static final int IS_FULL_TIME =2;
+	public static final int IS_PART_TIME =1;
+	public static final int EMP_RATE_PER_HOUR =20;
+	  
 	public static void main(String[] args) {
-		 final int IS_FULL_TIME =2;
-		 final int EMP_RATE_PER_HOUR =20;
-		 int IS_PART_TIME =1;
+		  
 		int empHrs=0;
 		int empWage=0;
-		double empCheck = Math.floor(Math.random()*10)%3;// for 0,1,2
-	
-		if (empCheck == IS_FULL_TIME) {
+		double empCheck =  Math.floor(Math.random()*10)%3;// for 0,1,2
+		
+		switch ((int)empCheck) {
+		    case IS_FULL_TIME:
 			empHrs=8;
 			System.out.println("Employee is present for full time");
-		}
-		else if (empCheck == IS_PART_TIME) {
+			break;
+		    case IS_PART_TIME: 
 			empHrs=4;
 			System.out.println("Employee is present for part time");
-		}
-		else{
+			break;
+		default: 
 			empHrs=0;
 			System.out.println("Employee is absent");
+			
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: "+empWage);
 	}
-
 }
+
