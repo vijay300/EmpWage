fef150c67cf50e38a0dc98c72957f3065c0e8cb9
vijay300 <gnvijayk@gmail.com>
2020-11-14 14:51:10 +0530

    uc3 added

--- src/com/practice/EmpWageBuilderUc1.java
+++ src/com/practice/EmpWageBuilderUc1.java
@@ -2,17 +2,22 @@ package com.practice;
 
 public class EmpWageBuilderUc1 {
 	public static void main(String[] args) {
-		 final int IS_FULL_TIME =1;
+		 final int IS_FULL_TIME =2;
 		 final int EMP_RATE_PER_HOUR =20;
+		 int IS_PART_TIME =1;
 		int empHrs=0;
 		int empWage=0;
-		double empCheck = Math.floor(Math.random()*10)%2;
+		double empCheck = Math.floor(Math.random()*10)%3;// for 0,1,2
 	
 		if (empCheck == IS_FULL_TIME) {
 			empHrs=8;
-			System.out.println("Employee is present");
+			System.out.println("Employee is present for full time");
 		}
-		else {
+		else if (empCheck == IS_PART_TIME) {
+			empHrs=4;
+			System.out.println("Employee is present for part time");
+		}
+		else{
 			empHrs=0;
 			System.out.println("Employee is absent");
 		}
