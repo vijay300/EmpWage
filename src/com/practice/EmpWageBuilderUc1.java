8c3ad874e31d4fd86923c88b23815eb3578428d2
vijay300 <gnvijayk@gmail.com>
2020-11-14 14:37:35 +0530

    UC1

---
+++ src/com/practice/EmpWageBuilderUc1.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+public class EmpWageBuilderUc1 {
+	public static void main(String[] args) {
+		final int IS_FULL_TIME =1;
+		 
+		double empCheck = Math.floor(Math.random()*10)%2;
+	
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else 
+			System.out.println("Employee is absent");
+	}
+
+}
